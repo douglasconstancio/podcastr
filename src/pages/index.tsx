72817027ee047f98eb6d42d0c9bc05630674d467
Douglas <douglas.constancio@ngi.com.br>
2021-04-23 02:43:20 -0300

    fix(durationTime): format duration time as string for view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import { GetStaticProps } from 'next'
+import { format, parseISO } from 'date-fns'
 import { api } from '../services/api'
+import { ptBR } from 'date-fns/locale'
+import { secondsToTimeString } from '../utils/secondsToTimeString'
 
 type Episode = {
     id: string
     title: string
+    thumbnail: string
+    description: string
     members: string
-    // ...
+    duration: string
+    durationAsString: string
+    url: string
+    publishedAt: string
 }
 
 type HomeProps = {
@@ -27,10 +35,25 @@ export const getStaticProps: GetStaticProps = async () => {
         { params: { _limit: 12, _sort: 'published_at', _order: 'desc' } }
     )
 
+    const episodes = data.map(episode => {
+        return {
+            id: episode.id,
+            title: episode.title,
+            thumbnail: episode.thumbnail,
+            members: episode.members,
+            publishedAt: format(
+                parseISO(episode.published_at),
+                'd MMM yy',
+                { locale: ptBR }
+            ),
+            duration: Number(episode.file.duration),
+            durationAsString: secondsToTimeString(Number(episode.file.duration)),
+            description: episode.file.url
+        }
+    })
+
     return {
-        props: {
-            episodes: data
-        },
+        props: { episodes },
         revalidate: 60 * 60 * 8 // In seconds
     }
 }
